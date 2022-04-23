62cc748f4fa50054c7e5441a93e6b4640d8f7c00
mainultalukder <98250337+mainultalukder@users.noreply.github.com>
2022-04-23 22:53:16 +0600

    Add files via upload

---
+++ games.js
@@ -0,0 +1,135 @@
+function show_more_less() {
+    var dots= document.getElementById('dots');
+    var moretext= document.getElementById('more');
+    var mybtn= document.getElementById('mybtn');
+
+    if (dots.style.display === 'none') {
+        dots.style.display='inline';
+        moretext.style.display='none';
+        mybtn.innerHTML = 'Show more';
+    }
+    else{
+        dots.style.display='none';
+        moretext.style.display='inline';
+        mybtn.innerHTML = 'Show less';
+    }
+}
+
+function show_more_less_1() {
+    var dot= document.getElementById('dot');
+    var moretext1= document.getElementById('more1');
+    var mybtn1= document.getElementById('mybtn1');
+
+    if (dot.style.display === 'none') {
+        dot.style.display='inline';
+        moretext1.style.display='none';
+        mybtn1.innerHTML = 'Show more';
+    }
+    else{
+        dot.style.display='none';
+        moretext1.style.display='inline';
+        mybtn1.innerHTML = 'Show less';
+    }
+}
+
+function show_more_less_2() {
+    var dot2= document.getElementById('dot2');
+    var moretext2= document.getElementById('more2');
+    var mybtn2= document.getElementById('mybtn2');
+
+    if (dot2.style.display === 'none') {
+        dot2.style.display='inline';
+        moretext2.style.display='none';
+        mybtn2.innerHTML = 'Show more';
+    }
+    else{
+        dot2.style.display='none';
+        moretext2.style.display='inline';
+        mybtn2.innerHTML = 'Show less';
+    }
+}
+
+function show_more_less_3() {
+    var dot3= document.getElementById('dot3');
+    var moretext3= document.getElementById('more3');
+    var mybtn3= document.getElementById('mybtn3');
+
+    if (dot3.style.display === 'none') {
+        dot3.style.display='inline';
+        moretext3.style.display='none';
+        mybtn3.innerHTML = 'Show more';
+    }
+    else{
+        dot3.style.display='none';
+        moretext3.style.display='inline';
+        mybtn3.innerHTML = 'Show less';
+    }
+}
+
+function show_more_less_4() {
+    var dot4= document.getElementById('dot4');
+    var moretext4= document.getElementById('more4');
+    var mybtn4= document.getElementById('mybtn4');
+
+    if (dot4.style.display === 'none') {
+        dot4.style.display='inline';
+        moretext4.style.display='none';
+        mybtn4.innerHTML = 'Show more';
+    }
+    else{
+        dot4.style.display='none';
+        moretext4.style.display='inline';
+        mybtn4.innerHTML = 'Show less';
+    }
+}
+
+function show_more_less_5() {
+    var dot5= document.getElementById('dot5');
+    var moretext5= document.getElementById('more5');
+    var mybtn5= document.getElementById('mybtn5');
+
+    if (dot5.style.display === 'none') {
+        dot5.style.display='inline';
+        moretext5.style.display='none';
+        mybtn5.innerHTML = 'Show more';
+    }
+    else{
+        dot5.style.display='none';
+        moretext5.style.display='inline';
+        mybtn5.innerHTML = 'Show less';
+    }
+}
+
+function show_more_less_6() {
+    var dot6= document.getElementById('dot6');
+    var moretext6= document.getElementById('more6');
+    var mybtn6= document.getElementById('mybtn6');
+
+    if (dot6.style.display === 'none') {
+        dot6.style.display='inline';
+        moretext6.style.display='none';
+        mybtn6.innerHTML = 'Show more';
+    }
+    else{
+        dot6.style.display='none';
+        moretext6.style.display='inline';
+        mybtn6.innerHTML = 'Show less';
+    }
+}
+
+function show_more_less_7() {
+    var dot7= document.getElementById('dot7');
+    var moretext7= document.getElementById('more7');
+    var mybtn7= document.getElementById('mybtn7');
+
+    if (dot7.style.display === 'none') {
+        dot7.style.display='inline';
+        moretext7.style.display='none';
+        mybtn7.innerHTML = 'Show more';
+    }
+    else{
+        dot7.style.display='none';
+        moretext7.style.display='inline';
+        mybtn7.innerHTML = 'Show less';
+    }
+}
